513ce4d4ed585623e8564d3c500c604a3ff573aa
nvqquy98 <nvqquy98@gmail.com>
2020-11-25 14:17:16 +0700

    Create structure project

---
+++ app/src/main/java/com/edu/movie/data/source/MovieDataSource.kt
@@ -0,0 +1,13 @@
+package com.edu.movie.data.source
+
+interface MovieDataSource {
+    /**
+     *  Local
+     */
+    interface Local
+
+    /**
+     *  Remote
+     */
+    interface Remote
+}
---
+++ app/src/main/java/com/edu/movie/data/source/local/MovieLocalDataSource.kt
@@ -0,0 +1,14 @@
+package com.edu.movie.data.source.local
+
+import com.edu.movie.data.source.MovieDataSource
+
+class MovieLocalDataSource : MovieDataSource.Local {
+
+    private object Holder {
+        val INSTANCE = MovieLocalDataSource()
+    }
+
+    companion object {
+        val INSTANCE: MovieLocalDataSource by lazy { Holder.INSTANCE }
+    }
+}
